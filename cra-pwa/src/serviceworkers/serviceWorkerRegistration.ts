e011eb0175a980062883b0677ecc99597002f66e
JoeM-RP <JMeyer@rightpoint.com>
2021-04-22 11:41:45 -0500

    path... again

--- cra-pwa/src/serviceworkers/serviceWorkerRegistration.ts
+++ cra-pwa/src/serviceworkers/serviceWorkerRegistration.ts
@@ -39,7 +39,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/serviceworkers/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/pwa-discovery/serviceworkers/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
