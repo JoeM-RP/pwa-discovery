b7524e1fa77f8d3b4af73b945bfb5e44432f59ae
JoeM-RP <JMeyer@rightpoint.com>
2021-04-22 13:03:57 -0500

    Reset

---
+++ cra-pwa-old/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+import { AppHeader } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader/>
+      <div className="App-body">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- cra-pwa/src/App.tsx
+++ cra-pwa/src/App.tsx
@@ -1,14 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-import { AppHeader } from './components';
 
 function App() {
   return (
     <div className="App">
-      <AppHeader/>
-      <div className="App-body">
-
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
